eb9f10c253d7d9be87c95f30f1fdf6e64cb4f538
Ashu Yadav <94470717+AshuYadav1@users.noreply.github.com>
2023-12-11 01:44:37 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -14,8 +14,8 @@ screen_height = w.winfo_screenheight()
 x_coordinate = (screen_width/2)-(width_of_window/2)
 y_coordinate = (screen_height/2)-(height_of_window/2)
 w.geometry("%dx%d+%d+%d" %(width_of_window,height_of_window,x_coordinate,y_coordinate))
-#w.configure(bg='#ED1B76')
-w.overrideredirect(1) #for hiding titlebar
+
+w.overrideredirect(1) 
 
 #new window to open
 def new_win():
@@ -26,11 +26,11 @@ label1=Label(w, text='Compliant Master', fg='white', bg='#272727')
 label1.configure(font=("Game Of Squids", 24, "bold"))   
 label1.place(x=80,y=90)
 
-label2=Label(w, text='Loading...', fg='white', bg='#272727') #decorate it 
+label2=Label(w, text='Loading...', fg='white', bg='#272727') 
 label2.configure(font=("Calibri", 11))
 label2.place(x=180,y=215)
 
-#making animation
+
 
 image_a=ImageTk.PhotoImage(Image.open('./assets/img1.png'))
 image_b=ImageTk.PhotoImage(Image.open('./assets/img2.png'))
@@ -38,7 +38,7 @@ image_b=ImageTk.PhotoImage(Image.open('./assets/img2.png'))
 
 
 
-for i in range(5): #5loops
+for i in range(5): 
     l1=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=180, y=145)
     l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=200, y=145)
     l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=220, y=145)
