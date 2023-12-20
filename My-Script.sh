527bc7826ee0dda0c18aa2481c27c1621d18c889
Ashutosh-upadhya <ashutosh.aku.aau@gmail.com>
2023-12-20 17:03:05 +0530

    Ashutosh Work

--- My-Script.sh
+++ My-Script.sh
@@ -301,3 +301,36 @@ disable_tor(){
 
 
 disable_tor
+blockIPV6() {
+
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+# If you want to make the changes persistent across reboots, you can edit /etc/sysctl.conf
+# and add the following lines:
+# net.ipv6.conf.all.disable_ipv6 = 1
+# net.ipv6.conf.default.disable_ipv6 = 1
+
+echo "IPv6 has been blocked."
+
+}
+
+blockIPV6
+
+blockWifi() {
+# Check if rfkill is available
+if command -v rfkill &> /dev/null
+then
+    # Block all wireless devices
+    sudo rfkill block wifi
+    echo "WiFi blocked."
+else
+    echo "rfkill command not found. Please install rfkill."
+    sudo apt install rfkill 
+
+fi
+
+
+}
+
+blockWifi
