25d1d6f97ecfde2b2a176e5c21a1c7b7fe940b39
vins4407 <vkesherwani000@gmail.com>
2023-12-20 16:53:10 +0530

    vinayaks work:integrated page

--- test7.py
+++ test7.py
@@ -1,7 +1,159 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import messagebox
 import json
+import customtkinter as ctk
+from tkinter import Listbox, END, MULTIPLE, messagebox, ttk
+import subprocess
+from CTkMessagebox import CTkMessagebox
+from customtkinter import CTk, filedialog
+import os
+import zipfile
+from helpers import *
+from tkinter import StringVar
+from ScriptGenerateTK  import *
+from generatePolicy import *
+
+
+
+def hide_all_screens():
+    for widget in app.winfo_children():
+        widget.grid_forget()
+
+
+
+def load_data(file_path):
+    try:
+        with open(file_path) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return {}
+
+
+def on_submit(data, selected_option_list):
+    
+
+    destination_path = os.getcwd()
+
+    try:
+        # Prompt user for the destination path to save the zip file
+        # destination_path = filedialog.askdirectory(title="Select Destination Folder")
+        destination_path = os.getcwd()
+
+        if destination_path:
+                with open("selectedFileName.txt", "w") as file:
+                # Write each selected item on a separate line
+                    for item in selected_option_list:
+                        file.write(f"{item}\n")
+
+
+            # Display a success message
+                CTkMessagebox(message="Success! file downloaded successfully.",
+                  icon="check", option_1="Done")
+
+    except Exception as e:
+        # Display an error message if something goes wrong
+        CTkMessagebox(title="Error", message=f"Something went wrong!!! \\n An error occurred: {str(e)}", icon="cancel")
+
+    if not selected_option_list:
+        CTkMessagebox(title="Warning Message!", message="Unable to connect!", icon="warning", option_1="Cancel", option_2="Retry")
+        return
+    show_confirmation_screen(selected_option_list,data)
+
+
+
+def show_confirmation_screen(selected_items,json_data):
+    popup = ctk.CTk()
+    popup.title("Confirmation")
+
+    window_width = 300
+    window_height = 200
+
+    screen_width = popup.winfo_screenwidth()
+    screen_height = popup.winfo_screenheight()
+
+    x_position = (screen_width - window_width) // 2
+    y_position = (screen_height - window_height) // 2
+
+    popup.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+
+    def download_Json_wrapper():
+        popup.destroy()
+        generateJson(selected_items,"data.json","Policy.json")
+
+    def harden_now_wrapper():
+        popup.destroy()
+        harden_now(selected_items,json_data)
+
+
+
+    download_button = ctk.CTkButton(popup, text="Download Policy", command=download_Json_wrapper )
+    download_button.pack( padx=10,pady=10)
+    
+    download_button = ctk.CTkButton(popup, text="Generate Script", command=generateScript )
+    download_button.pack( padx=10,pady=10)
+
+
+    hardening_button = ctk.CTkButton(popup, text="Start Hardening", command=harden_now_wrapper)
+    hardening_button.pack( padx=10,pady=10)
+
+    cancel_button = ctk.CTkButton(popup, text="Cancel",  command=popup.destroy )
+    cancel_button.pack(pady=10)
+
+    popup.mainloop()
+
+def harden_now(selected_items,json_data):
+    for selected_item in selected_items:
+        print(f"Hardening NOW for {selected_item}")
+        selected_id, selected_exec_file = find_selected_details(selected_item, json_data)
+        execute_script_and_display_result(selected_exec_file)
+
+def execute_script_and_display_result(exec_file):
+    output_frame = ctk.CTkFrame(app, corner_radius=3)
+    output_label=ctk.CTkLabel(output_frame,text="Script Execution Result", font=("Arial", 24))
+    output_label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
+  # Set the parent window
+
+    output_text = ctk.CTkTextbox(output_frame, wrap=ctk.WORD, width=500, height=300, font=("Arial", 12))
+    output_text.grid(row=1, column=0, padx=20, pady=10, sticky="w")
+    def show_frame():
+        hide_all_screens()  
+        output_frame.grid(row=0, column=0, padx=16, pady=20, rowspan=3, columnspan=3, sticky="nsew")
+        app.update()
+    
+    show_frame()
+
+    try:
+        current_dir = os. getcwd()
+        scriptFile = current_dir + "/" + exec_file
+        process = subprocess.run(['bash', exec_file], text=True, stdout=subprocess.PIPE, check=True)
+        output_text.insert(ctk.END, process.stdout)
+    except subprocess.CalledProcessError as e:
+        output_text.insert(ctk.END, f"Error executing script:\n{e}")
+
+    output_frame.grab_set()
+      # Make the output window modal
+
+
+def find_selected_details(selected_name, json_data):
+    for id, details in json_data.items():
+        if details['name'] == selected_name:
+            return id, details.get('execFile', 'Exec file not found')
+    return None, None
+
+
+
+def on_double_left_click(event, source_listbox, destination_listbox):
+    cur_index = source_listbox.nearest(event.y)
+    cur_item = source_listbox.get(cur_index)
+    source_listbox.delete(cur_index)
+    destination_listbox.insert(tk.END, cur_item)
+
+def exit():
+    app.destroy()
+    subprocess.run(["python3", "Dashboard.py", "--show-frame", "Options"])
+
+
 
 class CustomBox(tk.Canvas):
     def __init__(self, master, index, name, exec_file, is_special_case, callback):
@@ -77,7 +229,7 @@ class GUI:
             self.custom_boxes.append(custom_box)
             custom_box.grid(row=len(self.custom_boxes)//3, column=len(self.custom_boxes)%3, padx=10, pady=10)
 
-        submit_button = tk.Button(master, text='Submit', command=self.submit)
+        submit_button = tk.Button(master, text='Submit', command=lambda:on_submit(data, self.selected_objects),)
         submit_button.grid(row=2, column=0, pady=10)
 
         self.exec_files = []
@@ -120,9 +272,9 @@ class GUI:
             box.set_color()
             self.on_box_click(box)
 
-    def submit(self):
-        print(f"Selected objects: {self.selected_objects}")
-        print(f"Selected exec files: {self.exec_files}")
+
+
+
 
 def main():
     # Load data from JSON file
